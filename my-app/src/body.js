94078fe360bf5bc3fe24c52ee0060520c5e3cb3f
gedzilla <as8354093@gmail.com>
2024-02-25 17:01:41 +0800

    init

---
+++ my-app/src/body.js
@@ -0,0 +1,19 @@
+
+const Body = () => {
+    return <div>
+        <ul>
+            <li>Base Syntax & Rules</li>
+            <li>Variables, Values & Operators</li>
+            <li>Functions</li>
+            <li>Objects</li>
+            <li>Arrays</li>
+            <li>Control Structures</li>
+            <li>Browser APIs & The DOM</li>
+            <li>Essential Features Used By React</li>
+            <li>Tricky Parts</li>
+        </ul>
+    </div>
+}
+
+
+export default Body;
---
+++ my-app/src/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return <div className="footer">
+        <h1>2024/02/24</h1>
+    </div>
+}
+
+export default Footer;
