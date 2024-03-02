322a4ba30efbdc3f4aa3a6fd50f494eab02a0893
gedzilla <as8354093@gmail.com>
2024-03-02 10:56:06 +0800

    feat: dynamic jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -37,10 +37,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map(concept => <CoreConcept
+              image={concept.image}
+              description={concept.description}
+              title={concept.title} />)}
           </ul>
         </section>
         <section id="examples">
