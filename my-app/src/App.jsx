fff44a806ab08db07ee1d8e0a2f756f7c9c37cdf
gedzilla <as8354093@gmail.com>
2024-03-02 10:58:35 +0800

    feat: dynamic JSX with Spread Operator

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -37,10 +37,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map(concept => <CoreConcept
-              image={concept.image}
-              description={concept.description}
-              title={concept.title} />)}
+            {CORE_CONCEPTS.map(concept => <CoreConcept{...concept} />)}
           </ul>
         </section>
         <section id="examples">
